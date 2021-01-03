14996ed7c17b4e54a640564d0878cb2102e78869
LvChengbin <lvchengbin59@gmail.com>
2021-01-03 18:53:26 +0800

    removed src/init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,14 @@
 -------------------------------------------------------------------
  -- Copyright (C) 2021 LvChengbin
  --
- -- File: .hammerspoon/init.lua
+ -- File: init.lua
  -- Author: LvChengbin<lvchengbin59@gmail.com>
  -- Time: 01/03/2021
  -- Description:
 --------------------------------------------------------------------
 
-require( 'src' );
+package.path = debug.getinfo( 1, 'S' ).source:match[[^@?(.*[\/])[^\/]-$]] .. 'src/?.lua;' .. package.path
+
+require( 'hammerspoon-config-reload' );
+require( 'scroll' );
+require( 'control-escape' );
